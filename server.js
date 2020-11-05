9464fb769ef4aad5b19b67c8d5364bd8c52a4f89
Rômulo Andrade <56308434+andradeoromulo@users.noreply.github.com>
2020-11-04 22:53:51 -0300

    Add routes for /timestamp

--- server.js
+++ server.js
@@ -1,29 +1,58 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+// Routing
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/easteregg", function(req, res) {
+  res.json({greeting: "Oh, you've found this! Well, congrats! :p"});
 });
 
-// listen for requests :)
+app.get("/api/timestamp", function(req, res) {
+
+  const date = new Date();
+
+  res.json({
+    "unix": date.valueOf(),
+    "utc": date.toUTCString()
+  });
+
+});
+
+app.get("/api/timestamp/:dateParam", function(req, res) {
+
+  let dateParam = req.params.dateParam;
+
+  if (/^\d{5,}$/.test(dateParam))
+    dateParam = parseInt(dateParam);
+    
+  const date = new Date(dateParam);
+
+  if (date.toString() == "Invalid Date") {
+
+    res.json({
+      "error": "Invalid Date"
+    });
+
+  } else {
+
+    res.json({
+      "unix": date.valueOf(),
+      "utc": date.toUTCString()
+    });
+
+  }
+
+});
+
+// Listening for requests
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
